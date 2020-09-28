a2227e62d58836eef2755cf23de7eafdb70ad80e
Dobritoiu Diana <dianadobritoiu21@yahoo.com>
2020-09-28 20:33:19 +0300

    adaugat proiect initial lab1

---
+++ lab1/src/main/java/ro/utcluj/ssatr/lab1/ex2/Exercitiul2.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ro.utcluj.ssatr.lab1.ex2;
+
+/**
+ *
+ * @author DoubleD
+ */
+public class Exercitiul2 {
+    public static void main(String[] args) {
+        System.out.println("Hello2 !");
+    }
+}
