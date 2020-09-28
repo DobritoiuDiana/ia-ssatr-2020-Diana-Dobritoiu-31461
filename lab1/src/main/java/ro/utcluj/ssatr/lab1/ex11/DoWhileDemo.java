47c47cb8304ffec9a8fbaf0c75e6c28ca9831602
Dobritoiu Diana <dianadobritoiu21@yahoo.com>
2020-09-28 21:26:22 +0300

    Adaugare exercitii suplimentare

---
+++ lab1/src/main/java/ro/utcluj/ssatr/lab1/ex11/DoWhileDemo.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ro.utcluj.ssatr.lab1.ex11;
+
+/**
+ *
+ * @author DoubleD
+ */
+public class DoWhileDemo {
+    public static void main(String[] args){
+        int count = 1;
+        do {
+            System.out.println("Count is: " + count);
+            count++;
+        } while (count < 11);
+    }
+}
