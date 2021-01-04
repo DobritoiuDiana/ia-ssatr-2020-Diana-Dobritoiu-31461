e3c011bdc0be5e912e74fe0ab40fcbd76758678f
Dobritoiu Diana <dianadobritoiu21@yahoo.com>
2021-01-04 14:39:26 +0200

    laborator 3

---
+++ curs3-ssatr-ia/src/main/java/ro/utcluj/ssatr/curs3/ssatr/ia/colectii/retea/TestString.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ro.utcluj.ssatr.curs3.ssatr.ia.colectii.retea;
+
+import java.util.StringTokenizer;
+
+/**
+ *
+ * @author DoubleD
+ */
+public class TestString {
+    public static void main (String[] args){
+        String str ="17 + 66";
+        String token = "";
+        if(str.indexOf("+")!=-1)
+            token = "+";
+        if(str.indexOf("-")!=-1)
+            token = "-";
+        
+        //
+        StringTokenizer st = new StringTokenizer(str,"+");
+        
+        int op1 = Integer.parseInt(st.nextToken().trim());
+        int op2 = Integer.parseInt(st.nextToken().trim());
+        
+        int rez =  op1 + op2;
+        
+        String r = "Rezultat ="+rez;
+        
+        System.out.println(r);
+    }
+            
+}
